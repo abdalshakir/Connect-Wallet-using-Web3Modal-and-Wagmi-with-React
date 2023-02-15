2e9c25bc06e792a1ea21f2aa4cd81714b0e1b20b
Abdal Shakir <abdal_shakir715@hotmail.com>
2023-02-16 00:40:58 +0500

    resolve error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Routes } from './Routes/Routes'
+import Routes from './Routes/Routes'
 
 ReactDOM.render(
   <React.StrictMode>
