06616bc62c0c12d686957c3096aa19e1cb8175b6
Abdal Shakir <abdal_shakir715@hotmail.com>
2023-02-16 00:38:24 +0500

    resolve error

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes } from './Routes/Routes'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
+  document.getElementById('root')
 )
